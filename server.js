b4d72a1c5f141cc87596dfaa3c272127376a196f
Keshav Callychurn <keshav0799@gmail.com>
2025-06-29 15:23:57 -0400

    Add line to set views

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const PORT = 3000;
 app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
 
 app.get("/", (req, res) => {
   res.render("home", { page: "/" });
